14394e1c6f8c3a623eaf7f432fd0945f8681e24e
Todor Ivanov <ivanov.todor1@gmail.com>
2017-04-28 15:17:57 +0300

    Initial commit

---
+++ TaskManagerMVC/TaskManagerMVC/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagerMVC.Models
+{
+    public class Project: BaseModel
+    {
+        public string Name { get; set; }
+        public List<User> AssignedUSers { get; set; }
+
+        public List<Task> Tasks { get; set; }
+    }
+}
---
+++ TaskManagerMVC/TaskManagerMVC/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagerMVC.Models
+{
+    public class Task: BaseModel
+    {
+    }
+}
